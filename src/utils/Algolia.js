1eac13d47eef7e9d4daaa0942f1bbc4df6134282
Kamlesh9462G <kamleshgurjar950@gmail.com>
2023-06-27 16:16:36 +0530

    first commit

--- src/utils/Algolia.js
+++ src/utils/Algolia.js
@@ -1,29 +1,29 @@
-const algoliasearch = require("algoliasearch");
+// const algoliasearch = require("algoliasearch");
 
-const client = algoliasearch("9YXI7Z4ZD0", "20e9bd7f945d31ebbfd095fc31f18f4e");
+// const client = algoliasearch("9YXI7Z4ZD0", "20e9bd7f945d31ebbfd095fc31f18f4e");
 
-const index = client.initIndex("ObjectID");
+// const index = client.initIndex("ObjectID");
 
-// const record = { objectID: 1, name: "test_record" };
-const record = {
-  Name: "Ajay Gurjar",
-  EnquiryCourse: "Data Science",
-  CoursePrice: 30000,
-  Phone1: "9876543210",
-  Phone2: "1234567890",
-  Email: "tarun@gmail.com",
-  FollowupDate: null,
-  objectID: 859811,
-};
-
-
-const addLeadToAlgolia = async (leadData) => {
-    return await index.saveObject(leadData).wait();
-    return await index.saveObjects(leadData);
-}
+// // const record = { objectID: 1, name: "test_record" };
+// const record = {
+//   Name: "Ajay Gurjar",
+//   EnquiryCourse: "Data Science",
+//   CoursePrice: 30000,
+//   Phone1: "9876543210",
+//   Phone2: "1234567890",
+//   Email: "tarun@gmail.com",
+//   FollowupDate: null,
+//   objectID: 859811,
+// };
 
 
-module.exports = {
-  addLeadToAlgolia,
-};
+// const addLeadToAlgolia = async (leadData) => {
+//     return await index.saveObject(leadData).wait();
+//     return await index.saveObjects(leadData);
+// }
+
+
+// module.exports = {
+//   addLeadToAlgolia,
+// };
 
